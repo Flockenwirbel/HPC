4b1984d4c63ca32745ec45b5f61e80cd522bf035
Flockenwirbel <laurentlumiere1789@gmail.com>
2026-05-09 13:54:18 +0800

    fix(PA2): restore Phase 1 shared mem write-back and add prefetch optimization

--- PA2/apsp.cu
+++ PA2/apsp.cu
@@ -33,13 +33,27 @@ __global__ void phase1(int n, int k_start, int *graph) {
     __syncthreads();
 
     for (int k = 0; k < BS; ++k) {
+        int col_k[R], row_k[R];
+        for (int ri = 0; ri < R; ++ri)
+            col_k[ri] = shared[threadIdx.y * R + ri][k];
+        for (int rj = 0; rj < R; ++rj)
+            row_k[rj] = shared[k][threadIdx.x * R + rj];
+        for (int ri = 0; ri < R; ++ri) {
+            for (int rj = 0; rj < R; ++rj) {
+                reg[ri][rj] = min(reg[ri][rj], col_k[ri] + row_k[rj]);
+            }
+        }
+
+        // write back to shared mem
         for (int ri = 0; ri < R; ++ri) {
             for (int rj = 0; rj < R; ++rj) {
                 int posx = threadIdx.x * R + rj;
                 int posy = threadIdx.y * R + ri;
-                reg[ri][rj] = min(reg[ri][rj], shared[posy][k] + shared[k][posx]);
+                shared[posy][posx] = reg[ri][rj];
             }
         }
+
+        __syncthreads();
     }
 
     // write back to global mem
@@ -92,12 +106,14 @@ __global__ void phase2_row(int n, int kb, int *graph) {
 
 
     for (int k = 0; k < BS; ++k) {
+        int col_k[R], row_k[R];
         for (int ri = 0; ri < R; ++ri)
-            for (int rj = 0; rj < R; ++rj) {
-                int posx = threadIdx.x * R + rj;
-                int posy = threadIdx.y * R + ri;
-                reg[ri][rj] = min(reg[ri][rj], shared_pivot[posy][k] + shared_cur[k][posx]);
-            }
+            col_k[ri] = shared_pivot[threadIdx.y * R + ri][k];
+        for (int rj = 0; rj < R; ++rj)
+            row_k[rj] = shared_cur[k][threadIdx.x * R + rj];
+        for (int ri = 0; ri < R; ++ri)
+            for (int rj = 0; rj < R; ++rj)
+                reg[ri][rj] = min(reg[ri][rj], col_k[ri] + row_k[rj]);
     }
 
     // write back to global mem
@@ -152,12 +168,14 @@ __global__ void phase2_col(int n, int kb, int *graph) {
 
 
     for (int k = 0; k < BS; ++k) {
+        int col_k[R], row_k[R];
         for (int ri = 0; ri < R; ++ri)
-            for (int rj = 0; rj < R; ++rj) {
-                int posx = threadIdx.x * R + rj;
-                int posy = threadIdx.y * R + ri;
-                reg[ri][rj] = min(reg[ri][rj], shared_cur[posy][k] + shared_pivot[k][posx]);
-            }
+            col_k[ri] = shared_cur[threadIdx.y * R + ri][k];
+        for (int rj = 0; rj < R; ++rj)
+            row_k[rj] = shared_pivot[k][threadIdx.x * R + rj];
+        for (int ri = 0; ri < R; ++ri)
+            for (int rj = 0; rj < R; ++rj)
+                reg[ri][rj] = min(reg[ri][rj], col_k[ri] + row_k[rj]);
     }
 
     // write back to global mem
@@ -204,13 +222,16 @@ __global__ void phase3(int n, int kb, int *graph) {
         for (int rj = 0; rj < R; ++rj)
             reg[ri][rj] = (g_i + l_i + ri < n && g_j + l_j + rj < n) ? graph[(g_i + l_i + ri) * n + (g_j + l_j + rj)] : INF;
 
-    for (int k = 0; k < BS; ++k)
+    for (int k = 0; k < BS; ++k) {
+        int col_k[R], row_k[R];
         for (int ri = 0; ri < R; ++ri)
-            for (int rj = 0; rj < R; ++rj) {
-                int posx = threadIdx.x * R + rj;
-                int posy = threadIdx.y * R + ri;
-                reg[ri][rj] = min(reg[ri][rj], shared_col[posy][k] + shared_row[k][posx]);
-            }
+            col_k[ri] = shared_col[threadIdx.y * R + ri][k];
+        for (int rj = 0; rj < R; ++rj)
+            row_k[rj] = shared_row[k][threadIdx.x * R + rj];
+        for (int ri = 0; ri < R; ++ri)
+            for (int rj = 0; rj < R; ++rj)
+                reg[ri][rj] = min(reg[ri][rj], col_k[ri] + row_k[rj]);
+    }
 
     // write back to global mem
     for (int ri = 0; ri < R; ++ri)
